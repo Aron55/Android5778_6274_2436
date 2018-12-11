b1a298ee8aec6644ab93ddaa5f455814cff7cf77
יעקב אלבז <elbaz.yaacov@gmail.com>
2018-12-11 20:40:43 +0200

    A peut pres fini le controler

--- app/src/main/java/com/arons/android5778_6274_2436/MainActivity.java
+++ app/src/main/java/com/arons/android5778_6274_2436/MainActivity.java
@@ -2,59 +2,59 @@ package com.arons.android5778_6274_2436;
 
 import android.app.Activity;
 import android.content.ContentValues;
-import android.content.Context;
 import android.os.Bundle;
-import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.arons.android5778_6274_2436.Model.Backend.DBManager;
+import com.arons.android5778_6274_2436.Model.Backend.Const;
 import com.arons.android5778_6274_2436.Model.Backend.DBManager_Factory;
-import com.arons.android5778_6274_2436.Model.Datasource.Tools;
-import com.arons.android5778_6274_2436.Model.Entities.Classes.Location;
-import com.arons.android5778_6274_2436.Model.Entities.Classes.Ride;
-import com.arons.android5778_6274_2436.Model.Entities.Enum.RideType;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
 
-    private EditText name;
-    private EditText phone;
-    private EditText mail;
-    private EditText location;
-    private EditText arrival;
-    private Button button;
+    private EditText nameCus;
+    private EditText phoneCus;
+    private EditText mailCus;
+    private EditText locationCus;
+    private EditText destinationCus;
+    private Button buttonGet;
+    DBManager_Factory mydb ;
 
 
 
     private void findViews(){
-        name = (EditText)findViewById(R.id.editName);
-        phone = (EditText)findViewById(R.id.editPhone);
-        mail = (EditText)findViewById(R.id.editMail);
-        location = (EditText)findViewById(R.id.editLoc);
-        arrival = (EditText)findViewById(R.id.editArr);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(this);
+        nameCus = (EditText)findViewById(R.id.editName);
+        phoneCus = (EditText)findViewById(R.id.editPhone);
+        mailCus = (EditText)findViewById(R.id.editMail);
+        locationCus = (EditText)findViewById(R.id.editDep);
+        destinationCus = (EditText)findViewById(R.id.editDes);
+        buttonGet = (Button) findViewById(R.id.button);
+        buttonGet.setOnClickListener(this);
     }
 
     private void addAOrder(){
-            // a implementer
+        final ContentValues newRide = new ContentValues();
+        try {
+            newRide.put(Const.RideConst.NAME_OF_CUSTOMER,this.nameCus.getText().toString());
+            newRide.put(Const.RideConst.PHONE_OF_CUSTOMER,this.phoneCus.getText().toString());
+            newRide.put(Const.RideConst.MAIL_OF_CUSTOMER,this.mailCus.getText().toString());
+            newRide.put(Const.RideConst.DEPART_ADDRESS,this.locationCus.getText().toString());
+            newRide.put(Const.RideConst.DESTINATION_ADDRESS,this.destinationCus.getText().toString());
 
+            mydb.getInstance().addNewRide(newRide);
 
-            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DBManager_Factory mydb = new DBManager_Factory();
-        mydb.getInstance();
-
 
         findViews();
     }
--- app/src/main/java/com/arons/android5778_6274_2436/Model/Backend/DBManager.java
+++ app/src/main/java/com/arons/android5778_6274_2436/Model/Backend/DBManager.java
@@ -8,4 +8,6 @@ public interface DBManager {
     boolean addNewRide(ContentValues Ride, Context context);
 
     boolean updateRide(ContentValues Ride, Context context);
+
+    void addNewRide(ContentValues newRide); // Ajout par Yaacov , J'etais oblige de l'ajouter selon eux , j'espere ne pas avoir tout niquer ...
 }
