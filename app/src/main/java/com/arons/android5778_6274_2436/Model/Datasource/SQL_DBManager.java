ef1051fe4dddc0879de393c9dc81cbc5e19289ab
Aron Scemama <aronscemama55@gmail.com>
2019-01-08 17:16:46 +0200

    Nettoyage + changement de MyLocation

--- app/src/main/java/com/arons/android5778_6274_2436/Model/Datasource/SQL_DBManager.java
+++ app/src/main/java/com/arons/android5778_6274_2436/Model/Datasource/SQL_DBManager.java
@@ -1,18 +1,12 @@
 package com.arons.android5778_6274_2436.Model.Datasource;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.support.annotation.NonNull;
 
 import com.arons.android5778_6274_2436.Model.Backend.DBManager;
-import com.arons.android5778_6274_2436.Model.Backend.MapsFunction;
 import com.arons.android5778_6274_2436.Model.Entities.Classes.Ride;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class SQL_DBManager implements DBManager {
 
